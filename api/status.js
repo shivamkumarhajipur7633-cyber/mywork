099cffec79411f7557c38bbbfae46771f8f59d3e
shivamkumarhajipur7633-cyber <shivamkumarhajipur7633@gmail.com>
2026-08-03 16:21:42 +0530

    Remove api/status.js to convert project to pure static Vercel deployment

--- api/status.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({
-    message: "International SIM Sales Backend is running"
-  });
-};
